423c6661659320680a1a72ba27c212693f9d314a
hartmann78 <neizvestnik000@gmail.com>
2025-09-22 21:34:39 +0500

    new methods for mappers

--- src/main/java/com/practice/sharemate/mapper/CommentMapper.java
+++ src/main/java/com/practice/sharemate/mapper/CommentMapper.java
@@ -18,11 +18,27 @@ public class CommentMapper {
                 .build();
     }
 
+    public Comment dtoToEntity(CommentDTO commentDTO) {
+        return Comment.builder()
+                .id(commentDTO.getId())
+                .text(commentDTO.getText())
+                .created(commentDTO.getCreated())
+                .build();
+    }
+
     public List<CommentDTO> listToDto(List<Comment> comments) {
         List<CommentDTO> commentDTOS = new ArrayList<>();
         for (Comment comment : comments) {
             commentDTOS.add(entityToDto(comment));
         }
         return commentDTOS;
+    }
+
+    public List<Comment> listToEntity(List<CommentDTO> commentDTOS) {
+        List<Comment> comments = new ArrayList<>();
+        for (CommentDTO comment : commentDTOS) {
+            comments.add(dtoToEntity(comment));
+        }
+        return comments;
     }
 }
