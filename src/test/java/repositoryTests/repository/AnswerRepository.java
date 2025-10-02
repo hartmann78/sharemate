1c5766851b1c4f8c49d6aa1314f4781d7586c8ba
Alisher Bagitov <164620101+hartmann78@users.noreply.github.com>
2025-10-02 17:53:19 +0500

    Delete src/test/java/repositoryTests/repository directory

--- src/test/java/repositoryTests/repository/AnswerRepository.java
+++
@@ -1,7 +0,0 @@
-package repositoryTests.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import repositoryTests.model.Answer;
-
-public interface AnswerRepository extends JpaRepository<Answer, Long> {
-}
--- src/test/java/repositoryTests/repository/CommentRepository.java
+++
@@ -1,7 +0,0 @@
-package repositoryTests.repository;
-
-import repositoryTests.model.Comment;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CommentRepository extends JpaRepository<Comment, Long> {
-}
