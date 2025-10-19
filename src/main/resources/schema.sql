91c85fc4aa32cfba1da838201042715365e5a479
hartmann78 <neizvestnik000@gmail.com>
2025-10-19 13:21:56 +0500

    added delete cascade on several tables

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -20,45 +20,45 @@ create table users
 create table requests
 (
     id           serial primary key unique,
-    requestor_id bigint references users (id),
-    description  varchar(512)             not null,
-    created      timestamp with time zone not null,
-    updated      timestamp with time zone
+    requestor_id bigint references users (id) on delete cascade not null,
+    description  varchar(512)                                   not null,
+    created      timestamp with time zone                       not null,
+    updated      timestamp with time zone                       null
 );
 
 create table items
 (
     id           serial primary key unique,
-    name         varchar(255)                    not null,
-    description  varchar(512)                    not null,
-    is_available boolean                         not null,
-    owner_id     bigint references users (id)    not null,
-    request_id   bigint references requests (id) null
+    name         varchar(255)                                   not null,
+    description  varchar(512)                                   not null,
+    is_available boolean                                        not null,
+    owner_id     bigint references users (id) on delete cascade not null,
+    request_id   bigint references requests (id)                null
 );
 
 create table bookings
 (
     id         serial unique,
-    start_date timestamp with time zone,
-    end_date   timestamp with time zone,
-    booker_id  bigint references users (id),
-    item_id    bigint references items (id),
-    status     varchar not null
+    start_date timestamp with time zone                       not null,
+    end_date   timestamp with time zone                       not null,
+    booker_id  bigint references users (id) on delete cascade not null,
+    item_id    bigint references items (id) on delete cascade not null,
+    status     varchar                                        not null
 );
 
 create table comments
 (
     id        serial unique,
-    text      varchar(512),
-    item_id   bigint references items (id),
-    author_id bigint references users (id),
-    created   timestamp with time zone not null,
-    updated   timestamp with time zone
+    text      varchar(512)                                   not null,
+    item_id   bigint references items (id) on delete cascade not null,
+    author_id bigint references users (id) on delete cascade not null,
+    created   timestamp with time zone                       not null,
+    updated   timestamp with time zone                       null
 );
 
 create table answers
 (
     id         serial unique,
-    item_id    bigint references items (id),
-    request_id bigint references requests (id)
+    item_id    bigint references items (id) on delete cascade    not null,
+    request_id bigint references requests (id) on delete cascade not null
 );
